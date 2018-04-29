5ffcbe409ccbd2d1094528946c04b852e99550ce
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2018-04-29 21:59:58 +0200

    Add Travis and CodeClimate CI

---
+++ BuildScripts/osx_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+swift package generate-xcodeproj
+# Set pipefail to get status code of xcodebuild if it fails
+set -v -o pipefail
+# Test
+xcodebuild -enableCodeCoverage YES -project Swogic.xcodeproj -scheme Swogic test | xcpretty

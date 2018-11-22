d824319ce01babe472f5e28a02402f4551cdc17e
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2018-11-22 23:36:12 +0100

    Fix example

--- Examples/Sources/Examples/main.swift
+++ Examples/Sources/Examples/main.swift
@@ -29,23 +29,22 @@ enum HttpStatus: Equatable, EquatableAfterProjection {
     }
 }
 
-let login: Step<Void, HttpStatus>
-let request: Step<Void, HttpStatus>
-let refresh: Step<Void, HttpStatus>
-let logError: Step<HttpStatus, Void>
+let login: Step<Void, HttpStatus> = { .ok } ~ "login"
+let request: Step<Void, HttpStatus> = { .ok } ~ "request"
+let refresh: Step<Void, HttpStatus> = { .ok } ~ "refresh"
+let logError: Step<HttpStatus, Void> = { _ in } ~ "logError"
 
 // TODO: replace consume by operator
-let refreshAndRetry = Process([
+let refreshAndRetry: Process<Void, Void> = Process([
     refresh ---- { .ok            } ---> refresh.consume ---> request ---> request.consume,
     refresh ---- { .unauthorized  } ---> logError,
     refresh ---- { .internalError } ---> logError
 ])
 
-let process = Process([
-    login ---- { .ok            } ---> request ---- { .unauthorized  } ---> refreshAndRetry,
-    login ---- { .ok            } ---> request ---- { .internalError } ---> logError,
-    login ---- { .unauthorized  } ---> refreshAndRetry,
-    login ---- { .internalError } ---> logError
-
+let process: Process<Void, Void> = Process([
+    login ---- { .ok            } ---> login.consume ---> request ---- { .unauthorized  } ---> login.consume ---> refreshAndRetry,
+    login ---- { .ok            } ---> login.consume ---> request ---- { .internalError } ---> logError,
+    login ---- { .unauthorized  } ---> login.consume ---> refreshAndRetry,
+    login ---- { .internalError } ---> logError as StepChain<Void, Void>
     // TODO: think of a placeholder so we don't need to repeat login all the time
 ])
